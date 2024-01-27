bc1bf945ecb331f5d6b6424dde840f81df533952
sergiu porime <126587603+sergiu19996@users.noreply.github.com>
2024-01-27 14:56:15 +0000

    adding urls.py and channge views.py

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -3,5 +3,5 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.typedChoicesField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     override = forms.BooleanField(required=False, initial= False, widget=forms.HiddenInput)
--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -14,3 +14,11 @@ def cart_add(request, product_id):
         cd = form.cleaned_data 
         cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
     return redirect('cart:cart_detail')
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart':cart})
